b3678d04b39622089694dd3fa64a16b9dbc2fc02
Suvorov-rx <suvorov.rx@gmail.com>
2026-08-25 09:56:23 +0300

    Система контроля версий Git. Публикация проекта на GitHub

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Helo")
+def print_hi(name):
+    # Используйте точку останова (breakpoint) в строке ниже, чтобы отладить скрипт.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Нажмите зеленую кнопку Run в панели управления, чтобы запустить скрипт.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# Посетите руководство по PyCharm на https://www.jetbrains.com/help/pycharm/
